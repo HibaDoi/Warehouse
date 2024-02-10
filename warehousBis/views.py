3e4ef0ffb492e5f7062c4e7d6dba36ba5f8bd694
hiba doi <doihiba@iav.ac.ma>
2024-02-10 17:53:56 +0100

    some modification

--- warehousBis/views.py
+++ warehousBis/views.py
@@ -23,25 +23,45 @@ def get_temperature_data(request):
     # Retrieving the last 6 data points for the example
      # Replace 'your_streamname_id' with the ObjectId from your image
     streamname_id = ObjectId('65afebfa3a22637b465ed298')
-
+    R1=ObjectId('65afed703a22637b465ed29c')
+    R2=ObjectId('65afed983a22637b465ed29d')
+    R3=ObjectId('65afedb33a22637b465ed29e')
     # Retrieve the last 4 documents where 'streamname' matches 'streamname_id'
-    temperature_data = collection.find({'Streamname': streamname_id}).sort('_id', 1)
+    temperature_data_R1 = collection.find({'Streamname': streamname_id,'RoomID':R1}).sort('_id', 1)
+    temperature_data_R2 = collection.find({'Streamname': streamname_id,'RoomID':R2}).sort('_id', 1)
+    temperature_data_R3 = collection.find({'Streamname': streamname_id,'RoomID':R3}).sort('_id', 1)
 
     # Transforming the data into the format expected by Chart.js
-    hh = [([datetime.fromisoformat(data['PhenomenonTime']).strftime("%H:%M") ,data['Result'][0]])for data in temperature_data]
-    transformed_list = [[item[0] for item in hh], [item[1] for item in hh]]
-
+    hh1 = [([datetime.fromisoformat(data['PhenomenonTime']).strftime("%H:%M") ,data['Result'][0]])for data in temperature_data_R1]
+    hh2 = [([datetime.fromisoformat(data['PhenomenonTime']).strftime("%H:%M") ,data['Result'][0]])for data in temperature_data_R2]
+    hh3 = [([datetime.fromisoformat(data['PhenomenonTime']).strftime("%H:%M") ,data['Result'][0]])for data in temperature_data_R3]
+    transformed_list1 = [[item[0] for item in hh1], [item[1] for item in hh1]]
+    transformed_list2 = [[item[0] for item in hh2], [item[1] for item in hh2]]
+    transformed_list3 = [[item[0] for item in hh3], [item[1] for item in hh3]]
 
     # temperatures = [data['Result'] for data in temperature_data]
    
     data = {
         # 'labels': ["9AM", "10AM", "11AM", "12PM"],
-       'labels': transformed_list[0],
+       'labels': transformed_list1[0],
         'datasets': [{
             'label': 'Temperature',
-            'data': transformed_list[1],
+            'data': transformed_list1[1],
             # You can add more styling options here
-        }]
+        }],
+       'labels2': transformed_list2[0],
+        'datasets2': [{
+            'label2': 'Temperature',
+            'data_2': transformed_list2[1],
+            # You can add more styling options here
+        } ]     ,  
+       'labels_3': transformed_list3[0],
+        'datasets3': [{
+            'label3': 'Temperature',
+            'data3': transformed_list3[1],
+            # You can add more styling options here
+        } ]     ,         
+        
     }
     
     return JsonResponse(data)
@@ -141,7 +161,7 @@ def get_shelf_data(request):
     # gdf['rooms'] = np.repeat(['A', 'B', 'C'], [8, 8, 7])
     gdf['status'] = status
     # Retrieve the last 4 documents where 'streamname' matches 'streamname_id'
-    marchandise= collection1.find().limit(1)
+    marchandise= collection1.find().sort('_id', -1).limit(1)
     print(marchandise)
     marchandise = [da['id_thing'] for da in marchandise]
     room_merch= collection2.find({'_id':ObjectId(marchandise[0])}).sort('_id', -1).limit(1)
